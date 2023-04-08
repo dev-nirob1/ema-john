3f3713b2291b09148eda9b5bf5367b18d8a6c064
dev-nirob1 <dev-nirob1@gmail.com>
2023-04-08 10:28:09 +0600

    header responsive to be done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
