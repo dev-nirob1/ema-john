b98f548e9e92c27e6ad4c3d1f21067436039f88d
dev-nirob1 <dev-nirob1@gmail.com>
2023-04-26 20:40:24 +0600

    local storage bug fixed

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
-    let total = 0;
+    let totalPrice = 0;
     let totalShipping = 0;
     let quantity = 0;
-    for (const product of cart){
+    for (const product of cart) {
         // if(product.quantity === 0){
         //     product.quantity = 1;
         // }
-        
         // product.quantity = product.quantity || 1;
 
-        total = total + product.price * quantity;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
-        quantity = quantity +product.quantity;
+        quantity = quantity + product.quantity;
     }
-    const tax = total * 7 / 100;
-    const grandTotal = total + totalShipping + tax;
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + tax;
+
     return (
         <div className='cart'>
             <h5>Order summary</h5>
-            <p>Selected Items : {quantity}</p>
-            <p>Total Price : ${total}</p>
-            <p>Shipping Charge : ${totalShipping.toFixed(2)}</p>
-            <p>Tax : ${tax.toFixed(2)}</p>
-            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)} </h6>
         </div>
     );
 };
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -38,20 +38,22 @@ const Shop = () => {
     const handleAddToCart = (product) => {
 
         // const newCart = [...cart, product];
-        let newCart = []
-        //if product doesn't exist in the cart, then set quantity = 1
-        //if exist update the quantity by 1
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart, then set quantity = 1
+        // if exist update quantity by 1
         const exists = cart.find(pd => pd.id === product.id);
-        if (!exists) {
+        if(!exists){
             product.quantity = 1;
-            newCart = [...cart, product];
+            newCart= [...cart, product]
         }
-        else {
+        else{
             exists.quantity = exists.quantity + 1;
             const remaining = cart.filter(pd => pd.id !== product.id);
-            newCart = [...remaining, exists]
+            newCart = [...remaining, exists];
         }
-        setCart(newCart)
+
+        setCart(newCart);
         addToDb(product.id)
     }
 
